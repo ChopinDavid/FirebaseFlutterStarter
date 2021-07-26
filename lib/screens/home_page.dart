a557e7f69bf1eb2412d94bd1cd16b3927d1b7743
David Chopin <David.Chopin@wwt.com>
2021-07-25 19:10:48 -0500

    Removed BlocBuilder from HomePage - DC

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,17 +1,11 @@
-import 'package:firebase_flutter_starter/bloc/auth_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final AuthBloc _authBloc = AuthBloc();
-    return BlocProvider<AuthBloc>(
-      create: (_) => _authBloc,
-      child: Scaffold(
-        body: SafeArea(
-          child: Center(child: Text('This is the home page!')),
-        ),
+    return Scaffold(
+      body: SafeArea(
+        child: Center(child: Text('This is the home page!')),
       ),
     );
   }
